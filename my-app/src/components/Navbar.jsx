15b18566c486153257ede35be64c56b25d288fd6
Mrwaynejames <mcwilliamsjack96@gmail.com>
2023-01-16 17:53:55 -0800

    page styling

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
+import React, {useState}from 'react'
 import logo from '../assets/jm.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BaFillPersonLinesFill} from 'react-icons/bs'
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
     return (
-        <div className="fixed w-full h-[80px] flex jusitfy-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
             <div>
                 <img src={logo} alt="logo" style={{width:'50px'}} />
             </div>
             {/* menu */}
-            <div>
-                <ul className="flex">
+            
+                <ul className="hidden md:flex">
                     <li>Home</li>
                     <li>About</li>
                     <li>Skills</li>
                     <li>Work</li>
                     <li>Contact</li>
                 </ul>
-            </div>
+            
             {/* Hamburgrt */}
-            <div className="hidden">
-                <FaBars />
+            <div onClick={handleClick} className="md:hidden z-10">
+                {!nav ? <FaBars /> : <FaTimes />}
             </div>
             {/* mobil menu */}
-            <ul className="hidden">
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Work</li>
+                <li className='py-6 text-4xl'>Contact</li>
             </ul>
 
             {/* social icons */}
-            <div></div>
+            <div className='flex fixed flex-col top-[35%] left-0'>
+                <ul>
+                    <li>
+                        <a href="/">
+                            Linkedin <FaLinkedin size={30}/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
